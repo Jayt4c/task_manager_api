d0711801af8c1b36652ef0ed3e8995deefb820b5
Jayt4c <igejuwon53@gmail.com>
2024-10-21 11:56:27 +0100

    database redesigned for env

--- db.py
+++ db.py
@@ -1,14 +1,19 @@
+import os
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+
+load_dotenv()
 
 mysql = MySQL()
+secret_key = os.urandom(24)
 
 def init_db(app):
     mysql = MySQL(app)
     #configurations
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['USER'] = 'root'
-    app.config['PASSWORD'] = ''
-    app.config['MYSQL_DB'] = 'mydatabase'
-    app.secret_key = 'your_secret_key'
+    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+    app.config['MYSQL_DATABASE'] = os.getenv('MYSQL_DATABASE')
+    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+    app.secret_key = os.getenv('SECRET_KEY')
     
     return mysql
