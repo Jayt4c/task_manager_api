2c1f489f64579ae384f2265708deb6e82f137b57
Jayt4c <igejuwon53@gmail.com>
2024-10-21 11:54:55 +0100

    registered helpers i.e functions

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from routes import api
 from db import init_db
+from functions import register_helpers
 
+import os
 
 app = Flask(__name__)
+register_helpers(app)
 
 mysql = init_db(app)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'doc', 'docx', 'jpg', 'png'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 
 app.register_blueprint(api, url_prefix='/api')
 
